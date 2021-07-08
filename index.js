86bc785c5fca28ba2279f06d9d889b9e2c3c98c6
jrrohrer <jessie.rohrer@gmail.com>
2021-07-08 16:30:54 -0400

    commit tree 9a667ac111ac8406382232112d46195f96695e6b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, str) {
+  return drivers.filter( match => match.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter( match => { 
+    return match.toLowerCase().indexOf(str.toLowerCase()) === 0;
+  });
+}
+
+function matchName(drivers, str) {
+  return drivers.filter( match => {
+    return match.name === str;
+  });
+}
